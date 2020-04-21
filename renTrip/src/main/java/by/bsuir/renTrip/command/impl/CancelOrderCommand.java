a09634b2387eeda86af2ef4799bdd64f3914f501
Viacheslav <slavaguk2000@gmail.com>
2020-04-21 20:49:27 +0300

    set action on Cancel and Confirm button

---
+++ renTrip/src/main/java/by/bsuir/renTrip/command/impl/CancelOrderCommand.java
@@ -0,0 +1,40 @@
+package by.bsuir.renTrip.command.impl;
+
+import by.bsuir.renTrip.command.ActionCommand;
+import by.bsuir.renTrip.controller.ConfigurationManager;
+import by.bsuir.renTrip.controller.Router;
+import by.bsuir.renTrip.entity.Order;
+import by.bsuir.renTrip.repository.impl.BikeRepository;
+import by.bsuir.renTrip.repository.impl.ClientRepository;
+import by.bsuir.renTrip.repository.impl.OrderRepository;
+import by.bsuir.renTrip.repository.specification.bike.BikeUpdateStatusSpecification;
+import by.bsuir.renTrip.repository.specification.client.ClientUpdateDecrStatusSpecification;
+import by.bsuir.renTrip.repository.specification.order.OrderDeleteByIdSpecification;
+import by.bsuir.renTrip.type.PageChangeType;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@SuppressWarnings("unchecked")
+public class CancelOrderCommand implements ActionCommand {
+
+    @Override
+    public Router execute(HttpServletRequest request) {
+        Router router = new Router();
+        router.setPage(ConfigurationManager.getProperty("path.page.orders"));
+        router.setWay(PageChangeType.FORWARD);
+        int orderId = Integer.parseInt(request.getParameter("order_id"));
+
+        List<Order> orders = (List<Order>) request.getSession().getAttribute("orders");
+        Order order = orders.stream()
+                .filter(obj -> orderId == obj.getId())
+                .findFirst().orElse(null);
+
+        orders.remove(order);
+        OrderRepository.getInstance().update(new OrderDeleteByIdSpecification(orderId));
+        BikeRepository.getInstance().update(new BikeUpdateStatusSpecification(order.getBikeId(), 0));
+        ClientRepository.getInstance().update(new ClientUpdateDecrStatusSpecification(order.getUserId()));
+
+        return router;
+    }
+}
---
+++ renTrip/src/main/java/by/bsuir/renTrip/command/impl/ConfirmOrderCommand.java
@@ -0,0 +1,33 @@
+package by.bsuir.renTrip.command.impl;
+
+import by.bsuir.renTrip.command.ActionCommand;
+import by.bsuir.renTrip.controller.ConfigurationManager;
+import by.bsuir.renTrip.controller.Router;
+import by.bsuir.renTrip.entity.Order;
+import by.bsuir.renTrip.repository.impl.OrderRepository;
+import by.bsuir.renTrip.repository.specification.order.OrderUpdateStatusAndTimeByIdSpecification;
+import by.bsuir.renTrip.type.PageChangeType;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@SuppressWarnings("unchecked")
+public class ConfirmOrderCommand implements ActionCommand {
+
+    @Override
+    public Router execute(HttpServletRequest request) {
+        Router router = new Router();
+        router.setPage(ConfigurationManager.getProperty("path.page.orders"));
+        router.setWay(PageChangeType.FORWARD);
+        int orderId = Integer.parseInt(request.getParameter("order_id"));
+
+        List<Order> orders = (List<Order>) request.getSession().getAttribute("orders");
+        orders.stream()
+                .filter(obj -> orderId == obj.getId())
+                .findFirst().ifPresent(o -> o.setStatus(true));
+
+        OrderRepository.getInstance().update(new OrderUpdateStatusAndTimeByIdSpecification(orderId));
+
+        return router;
+    }
+}
